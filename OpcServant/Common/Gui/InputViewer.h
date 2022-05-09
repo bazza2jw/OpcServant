6ac9af97b059af46550664c045d4a082404ceb95
barry <bjh@micres.biz>
2022-05-09 19:07:12 +0100

    Moved InputViewer class to common

---
+++ OpcServant/Common/Gui/InputViewer.h
@@ -0,0 +1,27 @@
+#ifndef INPUTVIEWER_H
+#define INPUTVIEWER_H
+#include "InputViewUI.h"
+#include <MrlLib/mrllib.h>
+#include <Common/common.h>
+#include <Common/bobject.h>
+#include <Common/Gui/InputViewerHistory.h>
+#include <Common/reporter.h>
+#include <Common/Daq/rtobject.h>
+
+/*!
+ * \brief The InputViewer class
+ */
+class InputViewer : public InputViewerBase, public MRL::BObject
+{
+    static int _rId;
+    static constexpr int MaxHistorySize = 1000;
+    std::map<std::string,InputViewerHistory *> _history;
+public:
+    InputViewer(wxWindow* parent, unsigned id);
+    virtual ~InputViewer();
+    bool processQueueItem(const MRL::Message &msg);
+    void fetch(MRL::RtObjectRef &r); // load past hour of data
+protected:
+    virtual void DriveProcess(wxTimerEvent& event);
+};
+#endif // INPUTVIEWER_H
---
+++ OpcServant/Common/Gui/InputViewerHistory.h
@@ -0,0 +1,12 @@
+#ifndef INPUTVIEWERHISTORY_H
+#define INPUTVIEWERHISTORY_H
+#include "InputViewUI.h"
+
+class InputViewerHistory : public InputViewerHistoryBase
+{
+    static constexpr int VIEW_MAX_POINTS = 1000;
+public:
+    InputViewerHistory(wxWindow* parent);
+    virtual ~InputViewerHistory();
+};
+#endif // INPUTVIEWERHISTORY_H
